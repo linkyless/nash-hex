24baf6c72a473fa4052ca457f5d051c2eb775c70
linkyless <lestergamer200@gmail.com>
2026-08-12 19:57:17 +0200

    Add union-find disjoint sets with path compression and union by size

---
+++ hexzero/ufds.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class UFDS: 
+    def __init__(self, N: int):
+        self.num_sets = N
+        self.parent   = np.full(N, -1)
+
+
+    def find(self, cell: int) -> int:
+        if self.parent[cell] < 0:
+            return cell
+        
+        self.parent[cell] = self.find(self.parent[cell])
+        return self.parent[cell]
+
+
+    def related(self, first_cell: int, second_cell: int) -> bool:
+        return self.find(first_cell) == self.find(second_cell)
+
+
+    def join(self, first_cell: int, second_cell: int):
+        root_first_cell  = self.find(first_cell)
+        root_second_cell = self.find(second_cell)
+
+        if root_first_cell == root_second_cell:
+            return
+        if self.parent[root_first_cell] < self.parent[root_second_cell]:
+            root_first_cell, root_second_cell = root_second_cell, root_first_cell # Swap
+
+        self.parent[root_second_cell] = self.parent[root_second_cell] + self.parent[root_first_cell]
+        self.parent[root_first_cell]  = root_second_cell
+
+        self.num_sets = self.num_sets - 1
+
