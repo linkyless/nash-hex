c9183bcdc95b243cbe920578198238127ae8d73f
linkyless <lestergamer200@gmail.com>
2026-08-24 17:47:07 +0200

    Refactor: you can play now with Nash

--- hexzero/cli.py
+++ hexzero/cli.py
@@ -3,13 +3,16 @@ from hexzero.mcts import MCTS
 from hexzero.board import HexBoard
 import numpy as np
 import random
+import torch
  
  
 if __name__ == "__main__":
  
     board_size = 5
     
-    network = PolicyValueNetwork(board_size)
+    network = PolicyValueNetwork(5)
+    network.load_state_dict(torch.load("checkpoints/iter_29.pt"))
+    network.eval()
     zero_wins = 0
     matches = 50
     for _ in range(matches):
@@ -19,10 +22,10 @@ if __name__ == "__main__":
             Engine.print_board()
             player = Engine.current_player()       
             if player == 1: 
-                (row, col) = Engine.index_to_cell(random.choice(Engine.valid_choices()))
-                Engine.place(row, col)
+                row, col = map(int, input("Tu movimiento (fila columna): ").split())
+                Engine.place(row - 1, col - 1)
             else:
-                mcts = MCTS(Engine, 400, network)
+                mcts = MCTS(Engine, 800, network)
                 (_, move) = mcts.search()
                 (row, col) = Engine.index_to_cell(move)
                 Engine.place(row, col)
